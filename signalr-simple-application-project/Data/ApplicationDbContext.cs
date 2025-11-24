6df80d7ff272f8ac4c687d0f5719695c4d18c996
teodorp11 <teodor.posea@gmail.com>
2025-11-24 18:07:45 +0200

    Add project files.

---
+++ signalr-simple-application-project/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace signalr_simple_application_project.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
